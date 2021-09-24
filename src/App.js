cf03b1a01aa8786695f8627abbf92c3efbeb711c
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-24 15:28:00 +0530

    Added the outline nd useState hooks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color,setColor] = useState('');
+  const [error,setError] = useState(false);
+  const [list,setList] =  useState([]);
+  
+  return( <>
+  <section className="container">
+
+  </section>
+  <section className="colors">
+    Colors will show up here!
+  </section>
+  </>);
 }
 
 export default App
