8878f4f0b39ab14fd00ee393e3b98b04e7a1bf76
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-24 15:32:19 +0530

    completed the form outlay

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ function App() {
   const [color,setColor] = useState('');
   const [error,setError] = useState(false);
   const [list,setList] =  useState([]);
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello from console");
+  }
+
+
+
   return( <>
   <section className="container">
+    <h3>Color Generator!</h3>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={color} onChange = {(e) => setColor(e.target.value)} placeholder="#f15025" />
+      <button className="btn" type="submit">submit</button>
+    </form>
 
   </section>
   <section className="colors">
